51d03cab85dfe25328ebc2cfe43377e851dc10a9
pisces <chillin2@naver.com>
2024-02-24 13:21:52 +0900

    add gruvbox and dashboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,9 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.opt.fencs = { "cp949", "utf-8", "ucs-bom" }
+vim.scriptencoding = "utf-8"
+vim.opt.encoding = "utf-8"
+vim.opt.fencs = { "utf-8", "cp949", "ucs-bom" }
 
 vim.opt.autoindent = true
 vim.opt.smartindent = true
