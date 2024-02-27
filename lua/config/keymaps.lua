a79e93507f994051b3403da41bece9689bd1decd
pisces <chillin2@naver.com>
2024-02-27 18:50:59 +0900

    add lsp.lua and edit keymap.lua

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+local keymap = vim.keymap
+-- local opts = { noremap = true, silent = true }
+
+keymap.set("n", "x", '"_x')
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", 'vb"_d')
